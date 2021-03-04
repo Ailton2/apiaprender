6a84fcd6c1c1ab4e47ab3cf2748b34644b6f24b1
Ailton <tym.ailton@gmail.com>
2021-03-04 20:44:16 -0300

    iniciando projeto aprender

---
+++ src/main/java/br/com/apiaprender/ApiaprenderApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiaprender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiaprenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiaprenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apiaprender/ApiaprenderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiaprender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiaprenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
